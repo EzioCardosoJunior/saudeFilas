329ef3b51ae1c0aedd030fa222d075a771e88e27
EzioCardosoJunior <ejcj1980@msn.com>
2023-02-08 18:58:22 -0300

    ok

--- saudeFilasApp/src/app/components/home/home.component.ts
+++ saudeFilasApp/src/app/components/home/home.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+
+  public tiposServico!: FormGroup;
+
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    //this.tiposServico.value.TipoLista=1
+    
+    this.tiposServico = new FormGroup({
+      TipoServico: new FormControl(0)
+    })
+
+
+  };
+
+  buscaDadosPacienteListas() {
+    alert(this.tiposServico.value.TipoServico)
+  }
+
 
 }
